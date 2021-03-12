4ad09e906ed148b61ecbccfa7bb459c0322e1a3a
Kris van der Ven <kris@oodit.nl>
2021-03-12 13:13:38 +0100

    Add Lion Examples
    - install lion package
    - import lion components + library
    - add lion components to the index

---
+++ source/scripts/main.js
@@ -0,0 +1,6 @@
+import { LitElement, html } from 'lit-element';
+import { LionInput } from '@lion/input';
+import { LionButton } from '@lion/button';
+
+customElements.define('lion-input', LionInput);
+customElements.define('lion-button', LionButton);
